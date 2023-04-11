a78fb59da5bf2eea72517c414f504afead6c2f36
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-04-11 01:02:13 -0400

    Implement everything

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+  let all_args = std::env::args().collect::<Vec<_>>();
+  if all_args.len() == 1 {
+    println!("You need to give at least one argument");
+  } else {
+    let err = exec::Command::new(&all_args[1]).args(&all_args[2..]).exec();
+    println!("Something went wrong: {}", err);
+  }
 }
